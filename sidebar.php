42dbfabecbdc299f0ef624171c0a25fc304bf328
Niwagabaoscar <niwagabaoscar14@gmail.com>
2019-01-22 18:28:45 +0300

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<div class="sidebar-nav slimscrollsidebar">
+
     <div class="sidebar-head">
         <h3><span class="fa-fw open-close"><i class="ti-close ti-menu"></i></span> <span class="hide-menu">Navigation</span></h3> </div>
     <ul class="nav" id="side-menu">
